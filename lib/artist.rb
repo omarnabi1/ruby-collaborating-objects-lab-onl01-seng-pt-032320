36faf3b19f77b58d4afb4e401bb89a6a92549e3d
Omar Nabi <onabi20@gmail.com>
2020-04-20 05:10:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,18 +24,6 @@ def add_song(songs)
   @songs.push(songs)
 end
 
-
-# def self.find(name)
-#   self.all.detect {|artist| artist.name = name}
-# end
-
-# def self.create(name)
-#   artist = self.new
-#   artist.name = name
-#   artist
-# end
-# This class method should take the name that is passed in (remember, it will be a string), and do one of two things. Find the artist instance that has that name or create one if it doesn't exist. Either way, the return value of the method will be an instance of an artist with the name attribute filled out.
-
 def self.find_or_create_by_name(name)
 
 find_artist = self.all.find do |artist|
