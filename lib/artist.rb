2c1c90539f637941eeeef42821fe9b4e4ccb45e5
Omar Nabi <onabi20@gmail.com>
2020-04-20 05:06:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,42 @@
 class Artist 
-  
-  attr_accessor :name, :songs 
-  @@all = [ ]
-  
-  def initialize (name)
-    @name = name 
-    @songs = [ ] 
-  end
-  
-  def self.all 
-    @@all 
-  end
-  
-  def songs 
-    Song.all.select {|song| song.artist == self} 
-  end
-  
-  def add_song (songs)
-    songs.artist = self
-    @songs >> songs 
-  end
-  
-  def save
-    @@all << self 
-  end
-  
-  def self.find_or_create_by_name(name)
+@@all = []
+
+attr_accessor :name
+attr_reader :songs
+
+def initialize(name)
+ @name = name
+ @@all.push(self)
+ @songs = []
+end
+
+def self.all 
+  @@all
+end
+
+def songs
+    Song.all.select {|song| song.artist == self}
+end
+
+def add_song(songs)
+ 
+  songs.artist = self
+  @songs.push(songs)
+end
+
+
+# def self.find(name)
+#   self.all.detect {|artist| artist.name = name}
+# end
+
+# def self.create(name)
+#   artist = self.new
+#   artist.name = name
+#   artist
+# end
+# This class method should take the name that is passed in (remember, it will be a string), and do one of two things. Find the artist instance that has that name or create one if it doesn't exist. Either way, the return value of the method will be an instance of an artist with the name attribute filled out.
+
+def self.find_or_create_by_name(name)
 
 find_artist = self.all.find do |artist|
   artist.name == name
@@ -48,5 +59,3 @@ end
 
 
 end
-
-  
