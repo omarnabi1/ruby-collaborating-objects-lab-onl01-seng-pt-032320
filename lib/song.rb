ff9f78ebd920086c8b58d7a32858c33a46be1482
Omar Nabi <onabi20@gmail.com>
2020-04-20 05:08:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,38 @@
-class Song
+class Song 
 
-  attr_accessor :name, :artist
+attr_accessor :name, :artist
 
-  def initialize(name)
-    @name = name
-  end
+@@all = Array.new
 
-  def self.new_by_filename(file_name)
-    song = file_name.split(" - ")[1]
+def initialize(name)
+  @name = name
+  @@all.push(self)
+end
+
+def name
+  @name
+end
+
+def self.all
+  @@all
+end
+
+def self.new_by_filename(file_name)
+    
     artist = file_name.split(" - ")[0]
+    song = file_name.split(" - ")[1]
     new_song = self.new(song)
     new_song.artist_name = artist
     new_song
-  end
+ 
+end
 
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
+def artist_name=(name)
+self.artist = Artist.find_or_create_by_name(name)
+end
+
+def print_songs
+@songs
+end
 
 end
