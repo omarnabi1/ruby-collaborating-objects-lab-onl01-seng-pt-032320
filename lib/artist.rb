6e05faea9c475248c52ede2cf6ec26353e7aaed5
Omar Nabi <onabi20@gmail.com>
2020-04-20 05:07:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,50 +1,39 @@
-class Artist 
-@@all = []
+class Artist
 
-attr_accessor :name
-attr_reader :songs
+  attr_accessor :name, :songs
+  @@all =[]
 
-def initialize(name)
- @name = name
- @@all.push(self)
- @songs = []
-end
-
-def self.all 
-  @@all
-end
-
-def songs
-    Song.all.select {|song| song.artist == self}
-end
-
-def add_song(songs)
- 
-  songs.artist = self
-  @songs.push(songs)
-end
-
-
-def self.find_or_create_by_name(name)
-
-find_artist = self.all.find do |artist|
-  artist.name == name
+  def initialize(name)
+    @name = name
+    @songs = []
   end
-  
-  
-  if find_artist
-    find_artist
-  else
-    new_artist = self.new(name)
-    new_artist
+
+  def add_song(song)
+    @songs << song
   end
-end
 
-def print_songs
-  @songs.each do |song|
-   puts song.name
+  def save
+    @@all << self #saves the artist name to the all array
   end
-end
 
+  def self.all
+    @@all #displays the all array
+  end
 
+  def self.find_or_create_by_name(artist_name)
+    found_artist = self.all.find {|artist| artist.name == artist_name}
+    if found_artist
+      found_artist
+    else
+      new_artist = self.new(artist_name)
+      new_artist.save
+      new_artist
+    end
+  end
+
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
+  end
 end
