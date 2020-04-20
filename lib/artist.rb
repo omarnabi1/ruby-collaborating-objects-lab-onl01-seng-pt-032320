fec89127a9e722b92d1faca585a6331f46e80eca
Omar Nabi <onabi20@gmail.com>
2020-04-20 04:54:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,7 +17,22 @@ class Artist
   end
   
   def self.find_or_create_by_name (artist_name)
-    fou
+    found_artist = self.all.find {|artist| artist.name == artist_name}
+    if found_artist
+      found_artist
+    else
+      new_artist = self.new (artist_name)
+      new_artist.save
+      new_artist
+    end
 end
+  
+  def print_songs
+    @songs.each do |song|
+      puts song.name 
+    end
+  end
+end
+
 
   
