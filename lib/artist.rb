fc224d5bebbb2972358759b01eefc40d2f083032
Omar Nabi <onabi20@gmail.com>
2020-04-20 04:52:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,23 @@
 class Artist 
   
+  attr_accessor :name, :songs 
+  @@all = [ ]
+  
   def initialize (name)
     @name = name 
+    @songs = [ ] 
+  end
+  
+  def add_song
+    @songs << song 
+  end
+  
+  def save
     @@all << self 
   end
+  
+  def self.find_or_create_by_name (artist_name)
+    fou
 end
 
   
